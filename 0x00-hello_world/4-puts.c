94998f36efa27f3c20ea69e34eb9245ae729bc46
HAny12331 <hany1111mores@gmail.com>
2023-07-13 19:11:33 -0700

    kk

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - enter point
+ *
+ * Description: c program print with put
+ *
+ * Return: Always 0 (suc)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
