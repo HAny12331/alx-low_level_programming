3d07c35fafbcb8cfff53864b4770ff56ae76fac2
HAny12331 <hany1111mores@gmail.com>
2023-07-14 14:45:19 -0700

    0 file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
  * main - entry point
  *
- * Description: false or true validation
+ * Description: true or false validation.
  *
- * return: always(0)
+ * Return: always 0
  */
 int main(void)
 {
