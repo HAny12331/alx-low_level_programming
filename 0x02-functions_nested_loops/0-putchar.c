3db861576013d0d5ae894f1c4c40c3c5753d614f
HAny12331 <hany1111mores@gmail.com>
2023-07-18 15:30:56 -0700

    SFF

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: it print boot char
+ * 
+ * Return:0 (succsses)
+ */
+
+int main(void)
+{
+	printf("putchar\n");
+	return (0);
+}
