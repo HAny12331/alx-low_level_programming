370d57e754aa6a898f7371cb05531016963b8868
HAny12331 <hany1111mores@gmail.com>
2023-07-19 10:36:46 -0700

    Ffs

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - function to check if
- *		character is lowercase
+ * _islower - function to check if character is lowercase
  * @c: checks input of function
  *	otherwise always 0 (success)
  */
