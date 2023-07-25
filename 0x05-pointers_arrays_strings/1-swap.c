de749354e9f2c6ffd32b18f983bf5fe729427506
HAny12331 <hany1111mores@gmail.com>
2023-07-25 16:00:25 -0700

    fff

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * swap_int - swaps that values of two integers 
+ * busing two input parameters
+ *
+ * @a: intput parameter 1
+ * @b: input parameter 2
+ *
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
