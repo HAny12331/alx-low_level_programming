850ae7859ea05b2d1322a95127f49f558bb70edf
HAny12331 <hany1111mores@gmail.com>
2023-07-25 16:38:15 -0700

    gg

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - print a string, follwed by a newline to stdout
+ * _puts - prints a string, followed by a newline to stdout
  *
  * @str: string parameter to print
  *
- * Return: nothing
+ * Return: Nothing
 */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-	str++;
+		_putchar(*str + 0);
+		str++;
 	}
 	_putchar('\n');
 }
