2d1465ae1ee8c9b8131b668854971a2083e2e5fd
HAny12331 <hany1111mores@gmail.com>
2023-08-14 09:44:02 -0700

    a

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "doh.h"
+#include "stdlib.h"
+
+/**
+ * free_dog - frees yo dwags
+ * @d: yo dawg
+ *
+ * Return: void.
+ */
+void free_dog(dog_y *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
